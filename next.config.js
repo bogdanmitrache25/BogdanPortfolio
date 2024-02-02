2bb928e12c8f0c645dbbac9b90be97e86e5a5d54
Bogdan Mitrache <bogdanfreshh@gmail.com>
2024-02-02 19:50:49 +0100

    general bug fix

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
 module.exports = {
-  images: {
-    unoptimized: true,
-  },
-};
-
-// next.config.js
-
-module.exports = {
   reactStrictMode: true,
   distDir: "dist",
-  output: {
-    // Configura la exportación estática
-    export: true,
-  },
+  target: "serverless",
 };
