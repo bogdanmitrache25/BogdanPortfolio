acf2518a6342f1a7650c55826d052a24d0f357c3
Bogdan Mitrache <bogdanfreshh@gmail.com>
2024-02-02 19:29:18 +0100

    general bug fix

--- dist/static/chunks/webpack.js
+++ dist/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a3edb5c8e62d6ec2"; }
+/******/ 		__webpack_require__.h = function() { return "ad8053ccdb8abb63"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
