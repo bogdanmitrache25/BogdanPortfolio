9082ef50fa5dc99ba95b47971b8a3927f8792842
Bogdan Mitrache <bogdanfreshh@gmail.com>
2024-02-08 17:55:00 +0100

    add projects

--- Components/Projects.tsx
+++ Components/Projects.tsx
@@ -9,7 +9,6 @@ const Projects = () => {
     >
       <h1 className="heading">
         Pro<span className="text-yellow-400">YECTOS</span>
-        <span> ( ⚒️En Construcción ⚒️)</span>
       </h1>
       <div className="w-[80%] pt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[2rem]">
         <div data-aos="fade-up">
